954c00cd0e6939965ee5f1127b58f080f337190f
Frédérique Mittelstaedt <me@gfm.io>
2017-05-23 15:20:45 +0100

    Added Class Reps page

--- website/src/components/pages/whoweare/class-reps.js
+++ website/src/components/pages/whoweare/class-reps.js
@@ -6,27 +6,52 @@ module.exports = function(props, children) {
 
   return (
     <article class="uk-article">
-      <h1 class="uk-article-title">The Alumni Association e.V.</h1>
+      <h1 class="uk-article-title">Class Representatives</h1>
+
+      <h2>What is a Class Representative and who can become one?</h2>
+
       <p>
-        The Alumni Association was founded in June 2004 by the first Jacobs University graduating class. We are the growing network of (former) Jacobs University students and friends. The Alumni Association Board and the Alumni Officer work hard to provide you with the infrastructure to stay in touch with friends and former classmates, promote personal growth and give back to your alma mater. And we invite you to join us! Regular events, an annual Homecoming, Alumni Chapters in more and more cities around Europe and the Globe, an intranet with forums, profiles, and job offers, an Alumni Fund for donations (such as for scholarships), access to the gym and the IRC as well as numerous opportunities for participation provide added value for Alumni and the entire Jacobs University community, furthering commitment to Jacobs University.
-      </p>
+      Each class, who graduated from Jacobs University Bremen, elects their Class Representative. The initial election period is 3 years with subsequent election period to unlimited. The members of the Class can request new elections by submitting a petition supported by at least 10% of the members of the class.</p>
+      <p>The Class Representative is the elected key contact person of the respective alumni body as well as the key contact partner for the Board of the Alumni Association. The Class Representative is not merely a communications body and does not only function as a link between the Alumni Board and the Class. The Class Representative also serves to the best of interests of the alumni body. Across the years, the Class Representative will acquire new responsibilities and will be able to develop new initiatives on their own together with the other Class Representatives and with the Board. The Class Representative is expected to have self-motivation, communication drive, and Jacobs spirit.</p>
+      <p>The Class Representative can be anyone from any major and any graduation level. The Class Representative is a multiplier of ideas. The Class Representative is someone known to everyone and someone who knows everyone from their respective graduation class. The Class Representative is a communicator and a networker, an educator and a listener, a motivator and a mover.</p>
+      <p>Additionally: If you are having your 5-year Reunion this year, you will surely hear from your Class Representative and it is also the best person to contact if you have any questions.</p>
+
       <div class="uk-alert uk-alert-primary">
         <h3>
             <figure class="inline uk-margin-remove-bottom uk-margin-small-right">
-              <img src="/media/who-we-are/qlookingfor.png" />
+              <img src="/media/who-we-are/check.png" />
             </figure>
-          Looking for Someone?
+          Not yet Famous?
         </h3>
         <p>
-          Contact the Alumni Office! They are sure to be able to assist in finding your long lost roommate.
+          Contact the Alumni Office!  Donate and your name will be engraved in stone forever.
         </p>
       </div>
-      <h2>
-        Jacobs Alumni & Friends GmbH
-      </h2>
-      <p>
-        In early 2008 Jacobs Alumni & Friends GmbH was formally set up as a company owned 99% by the Alumni Association, with the remainder owned by Jacobs University. The purpose of the organization is to provide the legal framework for the alumni to become shareholders in the university. It is a new structure that comes as an extension of the existing Alumni Association. Jacobs Alumni & Friends GmbH has its own supervisory board, consisting of three alumni and three other members represented by figures from public life. It also sends a representative, i.e. the president of the Alumni Association e.V., to the two highest supervisory bodies, the Board of Trustees and the Board of Governors of the university. At this key position the alumni body is represented in the decision making process.
-      </p>
+
+      <h2>Why to have a Class Representative?</h2>
+      <p>Many alumni have expressed the desire to get involved in alumni affairs through different means than just donating. As our alumni body grows, however, it is challenging to do it in an organized way to make sure everyone’s voice counts. In 2011, the Class Representative position was launched as part of the organization of the Alumni Association to address this issue. The Alumni Board was keen to provide a structure that would strengthen the link between individual Alumni and build a more proactive Alumni Body.</p>
+
+      <h2>What to expect as a Class Representative:</h2>
+      <p>a) Tasks of a Class Representative:</p>
+      <p>A Class Representative participates in quarterly telcos and meetings with the Board and the designated Board Member. The Class Representative will share best practices with the other Class Representatives and seek their support and advice whenever necessary.</p>
+      <p>Fulfilling the mission of the Jacobs Alumni Association, and by extension of Jacobs University, the Class Representative is responsible, to various degrees of intensity, for:</p>
+      <ul>
+      <li>the support to fellow alumni through their first years after graduation</li>
+      <li>the expansion of the Alumni Association through integration of members of the class that are not members (e.g. assist the Board in Homecoming preparation and marketing)</li>
+      <li>the (metaphorical) mapping of knowledge and people distribution (e.g. assist alumni with contacts to other alumni, when they allocate to new cities or venture into new professions/projects )</li>
+      <li>the support to the Board in scholarship and reunion activities (e.g. project management of Scholarship Donation Campaign together with the Board as well as Reunion event management with the Board)</li>
+      <li>the support to the Alma Mater in admissions (e.g. conduct interviews and support prospective students get in touch with alumni across the globe)</li>
+      <li>the organization of the 5- year reunion of the class.</li>
+      </ul>
+      <p>b) Cooperation with the Board:</p>
+      <p>Of these general responsibilities, the Class Representative will be given clear work-package directions on an annual basis with quarterly and monthly progress reviews together with the designated Board Member. The concrete tasks will be open for discussion and further improvements are encouraged. Due to the seasonality of the needs of the members of specific classes after their years of graduation, the Class Representatives will all be at a different stage and will have a diverse set of projects tasks. Elected Class Representatives are expected to clearly indicate when they can no longer fulfill their duties. The Board reserve their right to demote a Class Representative if necessary.</p>
+      <p>c) As a Class Representative, you can expect:</p>
+      <ul>
+        <li>to stay connected with a variety of actors within the alumni body</li>
+        <li>to enjoy being close to your Class and your Alma Mater even if you are physically far away</li>
+        <li>to contribute significantly to the transparency of the work within the Alumni Board</li>
+        <li>to strengthen communication with the alumni body and to have a lot of fun while doing this.</li>
+      </ul>
     </article>
   )
 }
