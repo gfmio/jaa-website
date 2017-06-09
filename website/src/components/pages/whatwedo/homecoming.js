1565a2ed9aac0a5e3180f35ffdcd03fdbcba40dd
Frédérique Mittelstaedt <me@gfm.io>
2017-06-09 08:25:19 +0100

    Replaced placeholder pages with new placeholders

--- website/src/components/pages/whatwedo/homecoming.js
+++ website/src/components/pages/whatwedo/homecoming.js
@@ -6,11 +6,10 @@ module.exports = function(props, children) {
 
   return (
     <article class="uk-article">
-      <h1 class="uk-article-title">What we do</h1>
-
-      <p class="uk-text-lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
-
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h1 class="uk-article-title">Homecoming</h1>
+      <p class="uk-text-lead">
+        The Alumni Association was founded in June 2004 by the first Jacobs University graduating class.
+      </p>
     </article>
   )
 }
